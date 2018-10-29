b846ad2665e9c2e19f2111acb8f5a9767cdf4744
Hesbon <hesbonkiptoo5600@gmail.com>
2018-10-29 23:05:39 +0300

    [Feature #161420311] Create test for user login

--- app/api/v2/views.py
+++ app/api/v2/views.py
@@ -1,20 +1,18 @@
 from functools import wraps
 from flask import Flask, jsonify, make_response, request
 from flask_restful import Resource, Api
-from instance.config import Config
+from instance.config import app_config
 import jwt
 from .models import *
 from .utils import *
+from .models import User
 from werkzeug.security import check_password_hash
 import datetime
-import os
 
 
 def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
-        user_obj = User()
-        users = user_obj.get_all_users()
         token = None
         current_user = None
         if 'x-access-token' in request.headers:
@@ -24,14 +22,11 @@ def token_required(f):
                 'Message': 'Token is missing, You must login first'
             }), 401)
         try:
-            data = jwt.decode(
-                token, Config.SECRET_KEY, algorithms=['HS256'])
-
+            data = jwt.decode(token, app_config['development'].SECRET_KEY)
             for user in users:
                 if user['username'] == data['username']:
                     current_user = user
-        except Exception as e:
-            print(e)
+        except:
             return make_response(jsonify({'Message': 'Token is invalid'}),
                                  403)
         return f(current_user, *args, **kwargs)
@@ -56,4 +51,3 @@ class UserRegistration(Resource):
             'Message': "User '" + data['username'] +
             "' successfully registered as '" + data['role'],
         }), 201)
-
